4f0fa398cb1d8192f981a1ea634ea4b06b1752c7
Aidan Daquelente <ajd7040@psu.edu>
2024-12-01 22:04:28 -0500

    Check In 1
    Did not work on it over break, will go at it this week

--- avatar.js
+++ avatar.js
@@ -3,3 +3,10 @@ import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 import '@haxtheweb/rpg-character/rpg-character.js';
 
+// Need to implement RPG
+// Use checkbox for fire, slider for character size and dropdown for outfits
+// use a changedProperties function to keep track of changes done to character
+// Use media query to allow RPG to adjust to screen size
+// Use seeds to create variables that allow for functional URL
+
+
